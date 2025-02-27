ba228ba003ad63d0f6ff527cad466312f68f26f5
atharva11m <atharvamalode5@gmail.com>
2025-02-27 18:00:04 +0530

    #commit

---
+++ Prescripto/frontend/src/App.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import Doctors from "./pages/doctors";
+import Login from "./pages/Login";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Myprofile from "./pages/Myprofile";
+import MyAppointments from "./pages/MyAppointments";
+
+const App = () => {
+  return (
+    <div className="mx-4 sm:mx-[10%]">
+      <Routes>
+        <Route path="/" element={<Home></Home>} />
+        <Route path="/doctors" element={<Doctos>s</Doctos>}></Route>
+        <Route
+          path="/doctors/:speciality"
+          element={<Doctors></Doctors>}
+        ></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/contact" element={<Contact></Contact>}></Route>
+        <Route path="/my-profile" element={<Myprofile></Myprofile>}></Route>
+        <Route
+          path="/my-appointments"
+          element={<MyAppointments></MyAppointments>}
+        ></Route>
+        <Route
+          path="/appointment/:docId"
+          element={<MyAppointments></MyAppointments>}
+        ></Route>
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
